2f2d25378abaae3f9672e4b145a92b0fc3ed57d0
effgrandad <effgrandad@gmail.com>
2023-11-30 14:26:55 -0800

    import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     """print all hidden directories"""
     import hidden_4
 
-    for j in dir(hidden_4):
-        if j[:2] != "_":
-            print(j)
+     for name in sorted(dir(hidden_4)):
+        if name[:2] != '__':
+            print("{}".format(name))
+
