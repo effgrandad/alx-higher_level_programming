89375ddd410229f3d6eff22186a14cde919d1147
effgrandad <effgrandad@gmail.com>
2023-11-30 14:22:08 -0800

    import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+def add_arg(argv):
+    h = len(argv) - 1
+    if h == 0:
+        print("{:d}".format(h))
+        return
+    else:
+        m = 1
+        add = 0
+        while m <= h:
+            add += int(argv[m])
+            m += 1
+        print("{:d}".format(add))
+
+
 if __name__ == "__main__":
-    import sys, math
-    answer = 0
-    for j in range(len(sys.argv) - 1):
-        answer += int(sys.argv[j + 1])
-        print(answer)
+    import sys
+    add_arg(sys.argv)
+
 
