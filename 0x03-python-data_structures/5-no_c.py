2d2dd7417ed64d1342784dee2cfcd8f592e20e83
effgrandad <effgrandad@gmail.com>
2023-12-04 06:12:09 -0800

    data structures task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_e(my_string):
+    rot = ""
+    for x in range(len(my_string)):
+        if (my_string[x] != 'c' and my_string[x] != 'C'):
+            rot += my_string[x]
+        return rot
