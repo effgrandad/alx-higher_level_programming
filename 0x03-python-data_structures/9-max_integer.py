988d5e135a9b135da8411fa027fdf07f7d545474
effgrandad <effgrandad@gmail.com>
2023-12-05 11:34:34 -0800

    data structures and lists project

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) < 1:
-        return None
-    list_copy = my_list.copy()
-    list_copy.sort()
-    return list_copy(-1)
+    if len(my_list) == 0:
+        return ("None")
+    n = my_list[0]
+    for x in my_list:
+        if x > n:
+            n = x
+    return (n)
