262cfe9c0a48a5fc78757bf6426dfdc2cee7aeeb
effgrandad <effgrandad@gmail.com>
2023-12-04 18:35:49 -0800

    data structures and lists project

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0:
         return my_list
+    elif idx >= len(my_list):
+        return my_list
+    new_list = list(my_list)
+    new_list[idx] = element
+    return new_list
 
-    my_list_copy = my_list.copy()
-    my_list_copy[idx] = element
-    return my_list_copy
