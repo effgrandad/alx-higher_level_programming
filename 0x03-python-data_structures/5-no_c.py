c25cfc08969d72cd164511657e4c3fcff687043e
effgrandad <effgrandad@gmail.com>
2023-12-05 11:09:46 -0800

    data structures and lists project

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     rot = ""
-    for x in range(len(my_string)):
-        if (my_string[x] != 'c' and my_string[x] != 'C'):
-            rot += my_string[x]
-        return rot
+    for x in my_string:
+        if (x != 'c') and (x != 'C'):
+            rot += x
+        return (rot)
