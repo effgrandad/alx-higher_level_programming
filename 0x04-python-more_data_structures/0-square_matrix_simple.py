8ade55a60e343bc1af0ca74d9999665c986c3c4b
effgrandad <effgrandad@gmail.com>
2023-12-06 14:36:20 -0800

    data structures and lists project

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda submat: list(map(lambda s: s**2, sumat)), matrix))
+    ew_matrix = [row[:] for row in matrix]
+    for idx, row in enumerate(new_matrix):
+        for idx2, col in enumerate(new_matrix):
+            new_matrix[idx][idx2] = row[idx2]**2
+    return (new_matrix)
+
