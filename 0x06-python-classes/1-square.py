11d96004d45c1c118196a312235d284a1e8464fa
effgrandad <effgrandad@gmail.com>
2023-12-19 16:35:48 -0800

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,7 @@ class Square:
     """Defines a square."""
 
     def __init__(self, size):
-        """Constructor.
+        """Initialize a new Square.
 
         Args:
             Size: length of a side of the square.
