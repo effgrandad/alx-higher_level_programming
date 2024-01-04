774a4f4992e448536a40b398e5f8416a6ab7fa9f
effgrandad <effgrandad@gmail.com>
2024-01-04 12:25:53 -0800

    commits more class task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""
+Define a Rectangle class
+"""
+
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Representation of a rectangle."""
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
