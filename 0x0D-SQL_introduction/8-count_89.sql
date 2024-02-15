ea4d25ffa2f16d6baf39e1bf0df4cc773e9bee2e
effgrandad <effgrandad@gmail.com>
2024-02-15 12:59:02 -0800

    commit changes

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,4 @@
--- displays the quantity of records in the first_table on MySQL
-server that have id = 89
+-- displays the quantity of records in the first_table on MySQL server that have id = 89
 SELECT COUNT(*)
 FROM `first_table`
 WHERE `id` = 89;
