0de8f7054241b51d9ea468dd0931073e34d0512a
effgrandad <effgrandad@gmail.com>
2024-02-15 13:00:49 -0800

    commit changes

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
--- creates and adds several rows to the table second_table on my MySQL
-server with the characteristics id, name, and score
+-- creates and adds several rows to the table second_table on my MySQL server with the characteristics id, name, and score
 CREATE TABLE IF NOT EXISTS 'second_table' ('id' INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
