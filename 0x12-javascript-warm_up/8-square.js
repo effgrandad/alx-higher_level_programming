071de3d8330b162b9e3c6e4467da1beca1cd2a2b
effgrandad <effgrandad@gmail.com>
2024-03-12 06:43:57 -0700

    commit work on jsvascript warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   const x = Number(process.argv[2]);
   let y = 0;
   while (y < x) {
-    console.log("X".repeat(x));
+    console.log('X'.repeat(x));
     y++;
   }
 }
